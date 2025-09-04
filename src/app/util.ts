8455ae41b49fa7797f9053bcf972639403c47cf7
chibah-fawzi <fawzichibah@gmail.com>
2025-09-04 19:07:33 +0100

    added public env

--- src/app/util.ts
+++ src/app/util.ts
@@ -1,7 +1,7 @@
 let BACKEND_URI: string | undefined;
 
 if (process.env.NODE_ENV === "production") {
-  BACKEND_URI = process.env.BACKEND_URI;
+  BACKEND_URI = process.env.NEXT_PUBLIC_BACKEND_URI;
 } else {
   BACKEND_URI = "http://localhost:8000";
 }
